29522a0e78ff59a890fb5c52e041527eaec67446
Sohila Sayed <sohaila20191700306@cis.asu.edu.eg>
2022-04-26 22:50:44 +0200

    edit design

--- ElectionManagementSystem/ElectionManagementSystem/Form1.Designer.cs
+++ ElectionManagementSystem/ElectionManagementSystem/Form1.Designer.cs
@@ -143,6 +143,7 @@ namespace ElectionManagementSystem
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(161, 24);
             this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
