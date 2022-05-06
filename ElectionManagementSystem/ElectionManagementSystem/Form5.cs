41f388a117f5c306b35955fa6bf27af28af6a8eb
israaGaber245 <esraa20191700094@cis.asu.edu.eg>
2022-05-06 21:47:04 +0200

    crystal report 1

---
+++ ElectionManagementSystem/ElectionManagementSystem/Form5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectionManagementSystem
+{
+    public partial class Form5 : Form
+    {
+        CrystalReport1 CR;
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            CR=new CrystalReport1();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crystalReportViewer1.ReportSource = CR;
+        }
+    }
+}
